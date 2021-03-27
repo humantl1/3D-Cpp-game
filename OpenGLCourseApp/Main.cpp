87b6a006dc5c758d8d89a450fcbe013b99769d95
Tim Benton <timothybenton@gmail.com>
2021-03-27 09:37:12 -0700

    added translation matrix

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformModel;
+
+bool direction = true; // true for right
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.005f;
 
 // Vertex Shader
-static const char* vShader = "										\n\
-#version 330														\n\
-																	\n\
-layout (location = 0) in vec3 pos;									\n\
-																	\n\
-void main()															\n\
-{																	\n\
-		gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);	\n\
+static const char* vShader = "												\n\
+#version 330																\n\
+																			\n\
+layout (location = 0) in vec3 pos;											\n\
+																			\n\
+uniform mat4 model;														    \n\
+																			\n\
+void main()																	\n\
+{																			\n\
+		gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0); 	\n\
 }";
 
 // Fragment Shader
@@ -44,7 +55,7 @@ void CreateTriangle()
 
 		glGenBuffers(1, &VBO);
 			glBindBuffer(GL_ARRAY_BUFFER, VBO);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // this will be passed to the "in" variable
 
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 			glEnableVertexAttribArray(0);
@@ -114,8 +125,9 @@ void CompileShaders()
 		printf("Error validating  program: '%s'\n", eLog);
 		return;
 	}
+	
+	uniformModel = glGetUniformLocation(shader, "model"); // assigns id to uniformModel	
 }
-
 int main()
 {
 	// Initialize GLFW
@@ -175,11 +187,30 @@ int main()
 		// Get + handle user input events
 		glfwPollEvents();
 
+		if (direction)
+		{
+			triOffset += triIncrement;
+		}
+		else
+		{
+			triOffset -= triIncrement;
+
+		}
+		if (abs(triOffset) >= triMaxOffset)
+		{
+			direction = !direction;
+		}
+
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 			glUseProgram(shader);
+			
+			glm::mat4 model(1.0f);
+			model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+
+			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 				glBindVertexArray(VAO);
 

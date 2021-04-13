f7c38db301fcd458735835be8383e35cef02d851
Tim Benton <timothybenton@gmail.com>
2021-04-12 22:25:51 -0700

    implemented point lights and improved comments

--- OpenGLCourseApp/Main.cpp
+++ OpenGLCourseApp/Main.cpp
@@ -45,6 +45,8 @@ std::vector<Shader> shaderList;
 static const char* vShader = "Shaders/shader.vert";
 static const char* fShader = "Shaders/shader.frag";
 
+
+// Phong shading. Average normals of each face of shared vertices
 void calcAverageNormals(unsigned int* indices, // array of indices	
 	unsigned int indiceCount,	// size of indices array
     GLfloat* vertices,		// array of vertices
@@ -52,7 +54,7 @@ void calcAverageNormals(unsigned int* indices, // array of indices
 	unsigned int vLength,		// number of columns in vertices array
 	unsigned int normalOffset)	// offset from start of vertices array row to first normal index for that row
 {
-	// iterates through each face, averaging the normals
+	// iterates through each face, adding each face's normalized normal to vertices' normals
 	for (size_t i = 0; i < indiceCount; i += 3) // i is equivalent to the current face (aka triangle)
 	{
 		// assigns starting index of vertices array row corresponding to the vertex of the current triangle
@@ -74,6 +76,7 @@ void calcAverageNormals(unsigned int* indices, // array of indices
 		vertices[in2] += normal.x; vertices[in2 + 1] += normal.y; vertices[in2 + 2] += normal.z;
 	}
 
+	// normalize (average) vertices normals
 	for (size_t i = 0; i < verticeCount / vLength; i++) // for each row of vertices[]
 	{
 		unsigned int nOffset = i * vLength + normalOffset;										 // set index for start of normal indices for the ith row
@@ -85,31 +88,56 @@ void calcAverageNormals(unsigned int* indices, // array of indices
 
 void CreateObjects()
 {
-	// pyramid faces composed of vertex indices
-	unsigned int indices[] = {
-		0, 3, 1,
-		1, 3, 2,
-		2, 3, 0,
-		0, 1, 2
+	// Pyramids:
+	unsigned int indices[] = 
+	{
+		0, 3, 1, // left triangle
+		1, 3, 2, // right triangle
+		2, 3, 0, // front triangle
+		0, 1, 2  // bottom triangle
 	};
 
-	GLfloat vertices[] = {
+	GLfloat vertices[] = 
+	{
 		// x     y     z		 u     v		     normals
-		-1.0f, -1.0f, -0.6f,		0.0f, 0.0f,		0.0f, 0.0f, 0.0f, // bottom left
-		0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f, // "back"
-		1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f, // bottom right
-		0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f // top 
+		-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,		// bottom left
+		0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,		// "back"
+		1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,		// bottom right
+		0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f		// top 
 	};
 
+	// Floor:
+	unsigned int floorIndices[] =
+	{
+		0, 2, 1, // back left triangle
+		1, 2, 3  // front right triangle
+	};
+
+	GLfloat floorVertices[] =
+	{
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,	// back left
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,	// back right
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,	// front left
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f	// front right
+	};
+
+	// calculate averaged normals for pyramids
 	calcAverageNormals(indices, 12, vertices, 32, 8, 5);
 
+	// pyramid 1
 	Mesh *obj1 = new Mesh();
 	obj1->CreateMesh(vertices, indices, 32, 12);
 	meshList.push_back(obj1);
 
+	// pyramid 2
 	Mesh *obj2 = new Mesh();
 	obj2->CreateMesh(vertices, indices, 32, 12);
 	meshList.push_back(obj2);
+
+	// floor
+	Mesh* obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
 } 
 
 void CreateShaders()
@@ -126,55 +154,85 @@ int main()
 	CreateObjects();
 	CreateShaders();
 
-	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 5.00f, 0.2f);
 
+	// Set Textures
  	rockTexture = Texture((char*)"Textures/rock.png");
 	rockTexture.LoadTexture();
 	asteroidTexture = Texture((char*)"Textures/asteroid.png");
 	asteroidTexture.LoadTexture();
 
+
+	// Set Materials
 	shinyMaterial = Material(1.0f, 32);
 	dullMaterial = Material(0.3f, 4);
 
+
+	// Set Lighting
 	mainLight = DirectionalLight(1.0f, 1.0f, 1.0f,	// colors
 								0.1f, 0.3f,			// ambient & diffuse intensities 
 								0.0f, 0.0f, -1.0f);	// diffuse values
 
 	unsigned int pointLightCount = 0;
-	pointLights[0] = PointLight(0.0f, 1.0f, 0.0f,
-								0.1f, 1.0f,
-								-4.0f, 0.0f, 0.0f,
+	pointLights[0] = PointLight(0.0f, 0.0f, 1.0f,
+								0.1f, 0.4f,
+								4.0f, 0.0f, 0.0f,
 								0.3f, 0.2f, 0.1f);
 	pointLightCount++;
 
+	pointLights[1] = PointLight(0.0f, 1.0f, 0.0f,
+								0.1f, 1.0f,
+								-4.0f, 2.0f, 0.0f,
+								0.3f, 0.1f, 0.1f);
+	pointLightCount++;
+
+
+	// Initialize uniform variables
 	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0, 
 		uniformSpecularIntensity = 0, uniformShininess = 0;
 
+
+	// Set uniform values outside of main loop:
+
+	// initial camera values
+	camera = Camera(glm::vec3(0.0f, 0.0f, 0.0f),	// Camera start position
+		glm::vec3(0.0f, 1.0f, 0.0f),				// Camera start up direction
+		-90.0f,										// Camera start yaw
+		0.0f,										// Camera start pitch
+		5.00f,										// Camera move speed
+		0.2f);										// Camera turn speed
+
+	// projection matrix doesn't change
 	glm::mat4 projection = glm::perspective(
 		glm::radians(45.0f), // fov from top to bottom
 		mainWindow.getBufferWidth() / (GLfloat)mainWindow.getBufferHeight(), // aspect ratio
 		0.1f, // near z
 		100.0f); // far z
 
-	// Loop until window closed
+
+	// Main loop
 	while (!mainWindow.getShouldClose())
 	{
+		// Clear window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the color and depth buffers
+
+
 		// Delta Time
 		GLfloat now = glfwGetTime(); // returns time in seconds
 		deltaTime = now - lastTime;
 		lastTime = now;
+
 
 		// Get + handle user input events
 		glfwPollEvents();
 		camera.keyControl(mainWindow.getKeys(), deltaTime);
 		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
 
-		// Clear window
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the color and depth buffers
 
-		// 
-		shaderList[0].UseShader(); // start using the shader program
+		// start using shader program
+		shaderList[0].UseShader(); 
+
+		// assign uniform handles
 		uniformModel = shaderList[0].GetModelLocation();
 		uniformProjection = shaderList[0].GetProjectionLocation();
 		uniformView = shaderList[0].GetViewLocation();
@@ -182,17 +240,26 @@ int main()
 		uniformSpecularIntensity = shaderList[0].GetSpecularIntensityLocation();
 		uniformShininess = shaderList[0].GetShininessLocation();
 
+
+		// Set active lighting
 		shaderList[0].SetDirectionalLight(mainLight);
 		shaderList[0].SetPointLights(pointLights, pointLightCount);
 
+
+		// Set "universal" uniforms (projection, view, and eye position apply to entire scene)
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection)); // pass projection matrix to shader program
 		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix())); // pass camera matrix to shader program
 		glUniform3f(uniformEyePosition, camera.getCameraPosition().x, camera.getCameraPosition().y, camera.getCameraPosition().z);
 
-		// Initialize transformation matrix from triOffset. The next three line are just GLM, they aren't openGL. 
-		// The order of transformations is essentialy backwards
+
+		// Render Objects:
+
+		// triangle 1
+		// Initialize transformation matrix from triOffset.
 		glm::mat4 model(1.0f); // initialize simple 4x4 identity matrix using GLM
 		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f)); // glm function builds matrix to translate in direction and magnitude of vector
+
+		// Examples of rotate and scale matrices
 		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f)); // (matrix, angle of rotation, axis of rotation). This must concatenate the previous matrix
 		//model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f)); // (matrix, (scale coordinates))
 
@@ -206,15 +273,28 @@ int main()
 		shinyMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		meshList[0]->RenderMesh();
 		
+
+		// triangle 2
 		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, 3.0f, -2.5f)); // glm function builds matrix to translate in direction and magnitude of vector
-		//model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f)); // (matrix, (scale coordinates))
+		model = glm::translate(model, glm::vec3(0.0f, 3.0f, -2.5f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		asteroidTexture.UseTexture();
 		dullMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		meshList[1]->RenderMesh();
 
-		glUseProgram(0);
+
+		// floor
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		asteroidTexture.UseTexture();
+		dullMaterial.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		meshList[2]->RenderMesh();
+
+		// End Object Rendering.
+
+
+		glUseProgram(0); // clear shader program
 
 		mainWindow.swapBuffer();
 	}
--- OpenGLCourseApp/Mesh.cpp
+++ OpenGLCourseApp/Mesh.cpp
@@ -1,11 +1,7 @@
 #include "Mesh.h"
 
-Mesh::Mesh()
+Mesh::Mesh() : VAO{0}, VBO{0}, IBO{0}, indexCount{0}
 {
-	VAO = 0;
-	VBO = 0;
-	IBO = 0;
-	indexCount = 0;
 }
 
 void Mesh::CreateMesh(GLfloat *vertices, unsigned int *indices, unsigned int numOfVertices, unsigned int numOfIndices)
@@ -15,18 +11,24 @@ void Mesh::CreateMesh(GLfloat *vertices, unsigned int *indices, unsigned int num
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+		// indices array
 		glGenBuffers(1, &IBO);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
 			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * numOfIndices, indices, GL_STATIC_DRAW);
 
+			// vertices array
 			glGenBuffers(1, &VBO);
 			glBindBuffer(GL_ARRAY_BUFFER, VBO);
 				glBufferData(GL_ARRAY_BUFFER, sizeof(vertices[0]) * numOfVertices, vertices, GL_STATIC_DRAW);
 
+				// vertices sub-arrays
+				// x, y, z positions
 				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8, 0);
 				glEnableVertexAttribArray(0);
+				// texture u, v values
 		 		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8, (void*)(sizeof(vertices[0]) * 3));
 				glEnableVertexAttribArray(1);
+				// vertices normals
 		 		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]) * 8, (void*)(sizeof(vertices[0]) * 5));
 				glEnableVertexAttribArray(2);
 
